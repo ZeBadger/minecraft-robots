a4d0c70e537eec0dfaa366413b074024d44d061d
ZeBadger <github.com@zebadger.com>
2026-03-16 14:14:54 +0000

    Modernize code: Python 3 print, docstrings, style improvements

--- minecraft-robots.py
+++ minecraft-robots.py
@@ -15,133 +15,154 @@ import time
 import random
 
 #player class
+
 class Player:
+    """Represents the player in the Minecraft world."""
+
 
     def __init__(self, mc):
-        ppos=mc.player.getPos()
+        """Initialize player position and alive status."""
+        ppos = mc.player.getPos()
         self.alive = True
-        self.x=ppos.x
-        self.y=ppos.x
-        self.z=ppos.z
+        self.x = ppos.x
+        self.y = ppos.y
+        self.z = ppos.z
 
-    def wherePlayer(self,mc):
-        ppos=mc.player.getPos()
-        self.x=int(ppos.x)
-        self.y=int(ppos.y)
-        self.z=int(ppos.z)
-        print "p: x = " , self.x , " z = " , self.z , " y = " , self.y 
+    def wherePlayer(self, mc):
+        """Update player position and print it."""
+        ppos = mc.player.getPos()
+        self.x = int(ppos.x)
+        self.y = int(ppos.y)
+        self.z = int(ppos.z)
+        print(f"p: x = {self.x} z = {self.z} y = {self.y}")
 
 #robot class 
+
 class Robot:
-    robotCount=0
-    numDeadRobots=0
+    """Represents a robot enemy in the Minecraft world."""
+    robotCount = 0
+    numDeadRobots = 0
 
     def __init__(self, mc):
+        """Initialize robot at a random position."""
         self.mc = mc
         self.alive = True
-        self.x = random.randrange(-186,68)
+        self.x = random.randrange(-186, 68)
         self.y = 0
         self.y2 = 1
-        self.z = random.randrange(-63,191)
+        self.z = random.randrange(-63, 191)
         Robot.robotCount += 1
 
     def displayCount(self):
-        print "Number of robots = %d" % Robot.robotCount
+        """Print the total number of robots."""
+        print(f"Number of robots = {Robot.robotCount}")
 
     def whereRobot(self):
-        print "r: x = " , self.x , " z = " , self.z , " y = " , self.y , " y2 = " , self.y2 , " alive = ", self.alive
+        """Print the robot's current position and status."""
+        print(f"r: x = {self.x} z = {self.z} y = {self.y} y2 = {self.y2} alive = {self.alive}")
 
     def kill(self):
-        if (self.alive == True):
+        """Kill the robot and replace its blocks with stone."""
+        if self.alive:
             Robot.numDeadRobots += 1
             self.alive = False
-        self.mc.setBlock(self.x,self.y,self.z,block.STONE)
-        self.mc.setBlock(self.x,self.y2,self.z,block.STONE)
+        self.mc.setBlock(self.x, self.y, self.z, block.STONE)
+        self.mc.setBlock(self.x, self.y2, self.z, block.STONE)
         
 
-    def moveRobot(self,player):
-        if (self.alive == False):
+    def moveRobot(self, player):
+        """Move the robot towards the player and update its position in the world."""
+        if not self.alive:
             return
 
-        self.mc.setBlock(self.x,self.y,self.z,block.AIR)
-        self.mc.setBlock(self.x,self.y2,self.z,block.AIR)
+        # Remove robot's previous blocks
+        self.mc.setBlock(self.x, self.y, self.z, block.AIR)
+        self.mc.setBlock(self.x, self.y2, self.z, block.AIR)
 
-        if (player.x > self.x):
+        # Move towards player
+        if player.x > self.x:
             self.x += 1
-        if (player.x < self.x):
+        if player.x < self.x:
             self.x -= 1
 
-        if (player.y > self.y):
+        if player.y > self.y:
             self.y += 1
             self.y2 += 1
-
-        if (player.y < self.y):
+        if player.y < self.y:
             self.y -= 1
             self.y2 -= 1
 
-        if (player.z > self.z):
+        if player.z > self.z:
             self.z += 1
-        if (player.z < self.z):
+        if player.z < self.z:
             self.z -= 1
 
-        if (player.x == self.x) and ( (player.y == self.y) or (player.y == self.y2) ) and (player.z == self.z):
+        # Check if robot caught the player
+        if (player.x == self.x) and ((player.y == self.y) or (player.y == self.y2)) and (player.z == self.z):
             player.alive = False
 
-        self.mc.setBlock(self.x,self.y,self.z,block.IRON_BLOCK)
-        self.mc.setBlock(self.x,self.y2,self.z,block.NETHER_REACTOR_CORE)
+        # Place robot's new blocks
+        self.mc.setBlock(self.x, self.y, self.z, block.IRON_BLOCK)
+        self.mc.setBlock(self.x, self.y2, self.z, block.NETHER_REACTOR_CORE)
 
 
-#main program
+# Main program
 if __name__ == "__main__":
-
     mc = minecraft.Minecraft.create()
-    #constants
     player = Player(mc)
 
-    numRobots=10
-    robot = []
-    for r in range (0, numRobots):
-        robot.append(Robot(mc))
+    num_robots = 10
+    robots = []
+    for _ in range(num_robots):
+        robots.append(Robot(mc))
 
-    robot[0].displayCount()
-    
+    robots[0].displayCount()
+
+    # Game intro
     mc.postToChat("MCPI Robots! by ZeBadger (www.zebadger.com)")
-    mc.postToChat((""))
-    mc.postToChat(("I spy " + str(numRobots) + " robots coming to get you!"))
-    mc.postToChat(("They are unstoppable, except when they crash into"))
-    mc.postToChat(("each other.  Can you survive?"))
+    mc.postToChat("")
+    mc.postToChat(f"I spy {num_robots} robots coming to get you!")
+    mc.postToChat("They are unstoppable, except when they crash into")
+    mc.postToChat("each other.  Can you survive?")
     time.sleep(5)
-    mc.postToChat(("3..."))
+    mc.postToChat("3...")
     time.sleep(1)
-    mc.postToChat(("...2..."))
+    mc.postToChat("...2...")
     time.sleep(1)
-    mc.postToChat(("...1..."))
+    mc.postToChat("...1...")
     time.sleep(1)
-    mc.postToChat(("...Go!"))
+    mc.postToChat("...Go!")
     time.sleep(1)
-    while True :
+
+    # Main game loop
+    while True:
         player.wherePlayer(mc)
-        for r in range (0, numRobots):
-            robot[r].moveRobot(player)
-            robot[r].whereRobot()
-            # Now check collisions
-            for c in range (0, numRobots):
-                if (c != r):
-                    if (robot[r].x == robot[c].x) and ( (robot[r].y == robot[c].y) or (robot[r].y == robot[c].y2) ) and (robot[r].z == robot[c].z):
-                        robot[r].kill()
-                        robot[c].kill()
+        for r in range(num_robots):
+            robots[r].moveRobot(player)
+            robots[r].whereRobot()
+            # Check for robot collisions
+            for c in range(num_robots):
+                if c != r:
+                    if (
+                        robots[r].x == robots[c].x and
+                        (robots[r].y == robots[c].y or robots[r].y == robots[c].y2) and
+                        robots[r].z == robots[c].z
+                    ):
+                        robots[r].kill()
+                        robots[c].kill()
 
-        time.sleep(.3)
+        time.sleep(0.3)
 
-        if (player.alive != True):
+        if not player.alive:
             mc.postToChat("The robots got you!")
             break
 
-        if (Robot.numDeadRobots == numRobots):
+        if Robot.numDeadRobots == num_robots:
             mc.postToChat("You win!")
             break
 
-    for r in range (0, numRobots):
-        robot[r].kill()
+    # Clean up all robots
+    for r in range(num_robots):
+        robots[r].kill()
 
     mc.postToChat("Game Over")
